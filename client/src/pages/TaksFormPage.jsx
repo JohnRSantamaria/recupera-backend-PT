419ccb7444253a98d2567a3bbfa1ae01a551aaf4
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-08-01 12:43:01 -0500

    realizadano la escritura en la DB

--- client/src/pages/TaksFormPage.jsx
+++ client/src/pages/TaksFormPage.jsx
@@ -1,17 +1,36 @@
+import { useForm } from 'react-hook-form';
+import { createTasks } from '../api/tasks.api';
+import { useNavigate } from 'react-router-dom';
 export function TaksFormPage() {
+	const navigate = useNavigate();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm();
+
+	const onSubmit = handleSubmit(async (data) => {
+		await createTasks(data);
+		navigate('/tasks');
+	});
+
 	return (
 		<div>
-			<from>
+			<form onSubmit={onSubmit}>
 				<input
 					type='text'
 					placeholder='title'
+					{...register('title', { required: true })}
 				/>
+				{errors.title && <span>El titulo es requerido</span>}
 				<textarea
 					rows='3'
 					placeholder='description'
+					{...register('description', { required: true })}
 				></textarea>
+				{errors.description && <span>la descripcion es requerida</span>}
 				<button>Save</button>
-			</from>
+			</form>
 		</div>
 	);
 }
