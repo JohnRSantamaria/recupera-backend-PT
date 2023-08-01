55696675d25496741af1672764651d9b55f75eae
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-08-01 11:45:40 -0500

    agregando las rutas

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { TaskPage } from './pages/TaskPage';
+import { TaksFormPage } from './pages/TaksFormPage';
+import { Navigation } from './components/Navigation';
+function App() {
+	return (
+		<BrowserRouter>
+			<Navigation />
+			<Routes>
+				<Route
+					path='/'
+					element={<Navigate to='/tasks' />}
+				/>
+				<Route
+					path='/tasks'
+					element={<TaskPage />}
+				></Route>
+				<Route
+					path='/tasks-create'
+					element={<TaksFormPage />}
+				></Route>
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
---
+++ client/src/pages/TaksFormPage.jsx
@@ -0,0 +1,3 @@
+export function TaksFormPage() {
+	return <div>TaskFormPage</div>;
+}
